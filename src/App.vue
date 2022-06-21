9debff17de5df10d361399f5ed1065e5f7eeadd1
Вячеслав <foulin.tm@gmail.com>
2022-06-21 23:58:58 +0700

    №1 Шапка

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <nav-bar/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import NavBar from "@/components/NavBar.vue";
 
 @Component({
-  components: {
-    HelloWorld,
-  },
+  components: {NavBar},
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+
+}
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+*
+  margin: 0
+  padding: 0
+
 </style>
