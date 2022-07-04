28409f84257534cb47a17345fc22f32762abfcb2
Kovriga <60592893+Kovriga@users.noreply.github.com>
2022-07-04 22:40:06 +0700

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,29 @@
 <template>
   <div id="app">
     <nav-bar/>
+    <carousel :sliderItems="sliderItems"></carousel>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import {Component, Vue} from 'vue-property-decorator';
 import NavBar from "@/components/NavBar.vue";
+import Carousel from "@/components/carousel/Carousel.vue";
 
 @Component({
-  components: {NavBar},
+  components: {Carousel, NavBar},
 })
 export default class App extends Vue {
-
+  sliderItems: any = [
+    {
+      image: 'promo-01.png',
+      name:'promo-01',
+    },
+    {
+      image: 'promo-02.png',
+      name:'promo-02',
+    },
+  ]
 }
 </script>
 
@@ -21,4 +32,13 @@ export default class App extends Vue {
   margin: 0
   padding: 0
 
+body, html
+  height: 100%
+
+#app
+  height: 100%
+
+@font-face
+  font-family: 'Montserrat-Bold'
+  src: url("assets/fonts/Montserrat-Bold.woff") format('woff')
 </style>
